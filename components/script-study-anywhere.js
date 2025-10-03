a1c6fef6321c23e98595d84ada46c6d3cb832dc4
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2025-10-03 11:09:05 +0200

    adding component StudyAnywhere

---
+++ components/script-study-anywhere.js
@@ -0,0 +1,23 @@
+class StudyAnywhere extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+            <section class="study-anywhere">
+            <div class="studyAnywhere">
+                <!-- <div class="img"></div> -->
+                <img src="images/tablet_educatesmart-300x200.png" alt="">
+                <div class="studyAnywhereHeader">
+                    <h3>Study</h3>
+                    <h3>Anytime, Anywhere</h3>
+                    <!-- <h3>Anywhere</h3>  -->
+                    <h3>and on Any Platform</h3>
+                </div>
+            </div>
+            <div class="studyAnywhereText">
+                <h2>Smarter Training for Healthcare</h2>
+                <p>EduTechSmart equips healthcare companies with efficient, high-quality training tools. We offer affordable, flexible, and repeatable learning — available anytime, in multiple languages, on any device. With innovative exercises and a lower environmental impact, we go beyond traditional education.</p>
+            </div>
+
+        </section>`;
+  }
+}
+customElements.define('study-anywhere', StudyAnywhere);
